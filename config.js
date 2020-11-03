a0238cf3dadbf2f354386ecbbd776a6ca30d9725
amer13207 <71541192+amer13207@users.noreply.github.com>
2020-11-03 18:19:14 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    YT_API_KEY: 'api ', 
+    بادئة : "."
+    devs : [ '751426909669425243' ]
+}
---
+++ server.js
@@ -0,0 +1,182 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://.glitch.me/`);
+}, 280000);
+ 
+// كل البكجات الي ممكن تحتجها في اي بوت
+const { Client, RichEmbed } = require("discord.js");
+var { Util } = require('discord.js');
+const {YT_API_KEY, prefix, devs} = require('./config')
+const client = new Client({ disableEveryone: true})
+const ytdl = require("ytdl-core");
+const canvas = require("canvas");
+const Canvas = require("canvas");
+const convert = require("hh-mm-ss")
+const fetchVideoInfo = require("youtube-info");
+const botversion = require('./package.json').version;
+const simpleytapi = require('simple-youtube-api')
+const moment = require("moment");
+const fs = require('fs');
+const util = require("util")
+const gif = require("gif-search");
+const opus = require("node-opus");
+const ms = require("ms");
+const jimp = require("jimp");
+const { get } = require('snekfetch');
+const guild = require('guild');
+const dateFormat = require('dateformat');//npm i dateformat
+const YouTube = require('simple-youtube-api');
+const youtube = new YouTube('AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8');
+const hastebins = require('hastebin-gen');
+const getYoutubeID = require('get-youtube-id');
+const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
+const pretty = require("pretty-ms");
+client.login(process.env.BOT_TOKEN);
+const queue = new Map();
+var table = require('table').table
+const Discord = require('discord.js');
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+ 
+ 
+ //كود للتجربة
+
+
+
+let vipKeys = JSON.parse(fs.readFileSync("./vipKeys.json", "utf8"));
+client.on("message", msg=>{
+let id = "751426909669425243"; // ايديك
+let role = ""; // اسم الرتبه
+let Price = 10; // السعر
+let Price2 = 9
+let cmd = msg.content.split(' ')[0];
+if(cmd === `${prefix}buy`){
+if(msg.author.bot) return;
+if(!msg.channel.guild) return;
+let embedvip = new Discord.RichEmbed()
+.setColor("#42f4f4")
+.setAuthor(msg.author.username, msg.author.displayAvatarURL)
+.setThumbnail(msg.author.avatarURL)
+.setTitle("**اختر طريقة الشراء**")
+.addField("ل شراء الرتبه لنفسك","🔱",true )
+.addField("ل شراء الرتبه ك هدية","🎁",true)
+.setTimestamp()
+.setFooter(client.user.username,client.user.displayAvatarURL);
+msg.channel.send(embedvip).then(msgs2 =>{
+msgs2.react("🔱").then(()=>{
+  msgs2.react("🎁").then(()=>{
+    const me = (reaction, user) => reaction.emoji.name === '🔱' && user.id === msg.author.id;
+    const gift = (reaction, user) => reaction.emoji.name === '🎁' && user.id === msg.author.id;
+    const mec = msgs2.createReactionCollector(me, {time: 120000});
+    const giftc = msgs2.createReactionCollector(gift, {time: 120000});
+mec.on("collect", r=>{  
+msgs2.delete()
+if(msg.member.roles.find(r=>r.name == role)) return msg.reply("انت تمتلك الرتبة مسبقًا");
+let roleW = msg.guild.roles.find(r=>r.name == role);
+if(!roleW) return msg.reply(`البوت مقفل لعدم وجود رتبة ب أسم \`${role}\``)
+msg.channel.send(`كردت بروبوت\`${Price}\` لديك 4 دقائق لتحويل
+إلى ${msg.guild.members.get(id)}
+`).then( msgs =>{
+
+  const filter = response => response.author.id == "282859044593598464" && response.mentions._content.includes(`:moneybag: | ${msg.author.username}, has transferred \`$${Price2}\` to ${msg.guild.members.get(id)}`);
+msg.channel.awaitMessages(filter, { maxMatches: 1, time: 240000, errors: ['time'] })
+.then( collected =>{
+msgs.delete()
+msg.reply(`تم اعطائك رتبة \`${role}\``);
+msg.member.addRole(roleW);
+}).catch(e => {});
+})})
+giftc.on("collect", r=>{
+  msgs2.delete()
+  let roleW = msg.guild.roles.find(r=>r.name == role);
+  if(!roleW) return msg.reply(`البوت مقفل لعدم وجود رتبة ب أسم \`${role}\``)
+msg.channel.send(`كردت بروبوت\`${Price}\` لديك 4 دقائق لتحويل
+إلى ${msg.guild.members.get(id)}
+`).then( msgs =>{
+  const filter = response => response.author.id == "282859044593598464" && response.mentions._content.includes(`:moneybag: | ${msg.author.username}, has transferred \`$${Price2}\` to ${msg.guild.members.get(id)}`);
+  msg.channel.awaitMessages(filter, { maxMatches: 1, time: 240000, errors: ['time'] })
+  .then( collected =>{
+  msgs.delete()
+  genKey(msg,roleW);
+  }).catch(e => {});
+  })
+})
+})})})
+///
+}
+if(cmd === `${prefix}used`){
+  let args = msg.content.split(" ").slice(1)[0];
+  if(!args) {   
+    let embed = new Discord.RichEmbed()
+    .setColor("#42f4f4")
+    .setTitle(`:x: - **الرجاء ادخال كود الهدية** \`${prefix}used <Key>\``)
+    msg.reply(embed).then( z => z.delete(3000));
+    return
+}
+  let embed = new Discord.RichEmbed()
+.setTitle(`**جاري التحقق من الكود**`)
+.setColor("#42f4f4")
+  msg.reply(embed).then( msgs =>{
+  if(vipKeys[args]){
+    let hav = msg.member.roles.find(`name`, vipKeys[args].name);
+    if(hav){
+    let embed = new Discord.RichEmbed()
+    .setTitle(`:x: - **انت تمتلك هذه الرتبة مسبقًا**  \`${vipKeys[args].name}\``)
+    .setColor("#42f4f4")
+    msgs.edit(embed)
+    return
+    }
+    let embed = new Discord.RichEmbed()
+    .setTitle(`:tada: - **مبروك تم اعطائك رتبة** \`${vipKeys[args].name}\``)
+    .setColor("#42f4f4")
+    msgs.edit(embed)
+    msg.member.addRole(vipKeys[args]);
+    delete vipKeys[args]
+    save()
+  }else{
+    let embed = new Discord.RichEmbed()
+    .setTitle(`:x: - **الكود غير صيحيح أو انه مستعمل من قبل**`)
+    .setColor("#42f4f4")
+    msgs.edit(embed)
+  }});
+}
+});
+
+function genKey(msg,role){
+  var randomkeys = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var gift = "";
+  for (var y = 0; y < 16; y++) {   ///16
+    gift +=  `${randomkeys.charAt(Math.floor(Math.random() * randomkeys.length))}`;
+  }
+  vipKeys[gift] = role;
+  let embed = new Discord.RichEmbed()
+  .setColor("#42f4f4")
+  .setTitle(`:ok_hand: - **تم ارسآل الكود على الخاص**`);
+  msg.reply(embed);
+  let embed2= new Discord.RichEmbed()
+  .setAuthor(msg.author.username, msg.author.displayAvatarURL)
+  .setThumbnail(msg.author.avatarURL)
+  .addField("**Key Of Gift**", gift,true)
+  .addField("Role",vipKeys[gift].name,true)
+  .addField("This Key Made by", msg.author, true)
+  .addField("The Room", msg.channel , true)
+  .setTimestamp()
+  .setFooter(client.user.username,client.user.displayAvatarURL)  
+  msg.author.send(embed2);
+  save()
+}
+
+function save(){
+  fs.writeFile("./vipKeys.json", JSON.stringify(vipKeys), (err) => {
+    if (err) console.log(err)
+  });
+
+}
+
